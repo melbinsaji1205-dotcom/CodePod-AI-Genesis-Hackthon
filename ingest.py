46c684770132edb87f9b48dac2d61eadc5ad810f
melbinsaji1205-dotcom <melbinsaji1205@gmail.com>
2025-11-17 11:40:03 +0400

    Rename file from ingest_to_qdrant.py to ingest.py

--- ingest.py
+++ ingest.py
@@ -8,7 +8,7 @@ from sentence_transformers import SentenceTransformer
 from sklearn.preprocessing import normalize
 import numpy as np
 
-QDRANT_URL = os.getenv("http://localhost:6333", None)  
+QDRANT_URL = os.getenv("", None)  
 QDRANT_API_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.P9qf7bJaV2hpzpkgIUlusSFVyJLUz3jHrXDI8X6SMgU", None)
 COLLECTION_NAME = "db"
 
@@ -69,6 +69,7 @@ if points:
     client.upsert(collection_name=COLLECTION_NAME, points=points)
 
 print("Ingestion complete.")
+
 
 
 
